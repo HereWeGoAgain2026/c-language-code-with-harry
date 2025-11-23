22ff80bd1293597098c185fc5a15ccf4905a2de0
Yash Bhamare <yashnew2023@gmail.com>
2025-11-23 12:13:07 +0530

    Create 04_character.c

---
+++ chapter 7 Arrays/04_character.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+   char a = 'A';
+    char *ptr = &a;
+    printf("The address of a is %u\n", &a);
+    printf("The address of a is %u\n", ptr);
+    ptr++;
+    printf("The value of ptr is %u\n", ptr);
+    return 0;
+}
